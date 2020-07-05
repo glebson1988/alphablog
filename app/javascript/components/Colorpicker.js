9e128678d0a4b505b9394297342767d81cddd4fc
Gleb Grishakov <glebson1988@yandex.ru>
2020-07-05 16:23:15 +0300

    adding handleChange func to Colorpicker

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -3,13 +3,25 @@ import PropTypes from "prop-types"
 import { SketchPicker } from "react-color"
 
 class Colorpicker extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (color) => {
+        document.body.style.background = color.hex;
+    }
+
     render() {
-        return (<React.Fragment>
-            <SketchPicker />
-        </React.Fragment>);
+        return(
+            <React.Fragment>
+                <SketchPicker color={this.props.color}
+                              onChange={this.handleChange} />
+            </React.Fragment>);
     }
 }
 
-Colorpicker.PropTypes = {};
+Colorpicker.propTypes = {};
 
 export default Colorpicker
